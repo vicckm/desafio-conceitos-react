bfff7c5085dffd99bfd130d5c139bd3bf8b4c81d
Victoria Marques <vicck.rj@gmail.com>
2020-11-13 15:37:49 -0300

    challenge done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
 import React from "react";
-
+import api from "./services/api";
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = React.useState([]);
+
+  React.useEffect(() => {
+    api.get("repositories").then((response) => {
+      setRepositories(response.data);
+    });
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post("repositories", {
+      title: "Desafio Node.js",
+      url: "https://github.com/Rocketseat/unform",
+      techs: ["Node.js", "React.js"],
+    });
+
+    const repository = response.data;
+    setRepositories([...repositories, repository]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`repositories/${id}`);
+    setRepositories(repositories.filter((repository) => repository.id !== id));
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map((repository) => {
+          return (
+            <li key={repository.id}>
+              {repository.title}
+              <button onClick={() => handleRemoveRepository(repository.id)}>
+                Remover
+              </button>
+            </li>
+          );
+        })}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
